f7008b363a80806981a9a82a80e01200cc15ddb1
CodyAdam <cody.ad4@gmail.com>
2021-03-17 17:01:16 +0100

    Add spawn of messengers

--- src/spawnManager.ts
+++ src/spawnManager.ts
@@ -1,6 +1,6 @@
 export class SpawnManager {
-  public static roles: string[] = ["miner", "haulier", "upgrader", "repairer", "builder", "claimer"];
-  public static targetCount: number[] = [6, 8, 3, 1, 3, 1];
+  public static roles: string[] = ["miner", "haulier", "upgrader", "repairer", "builder", "claimer", "messenger"];
+  public static targetCount: number[] = [6, 8, 3, 1, 3, 1, 1];
   public static maxCount = 30;
 
   public static spawn(spawn: StructureSpawn): string {
