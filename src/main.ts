7f721b0e6c448b0987967cb043a1a2d4ba42751c
CodyAdam <cody.ad4@gmail.com>
2021-03-17 09:52:48 +0100

    Fix tower

--- src/main.ts
+++ src/main.ts
@@ -15,66 +15,62 @@ global.cmd = cmd;
 global.MINER_PER_SOURCE = 1;
 
 export const loop = ErrorMapper.wrapLoop(() => {
-    if (Game.cpu.bucket >= 10000) {
-        Game.cpu.generatePixel();
+  if (Game.cpu.bucket >= 10000) {
+    Game.cpu.generatePixel();
+  }
+
+  var tower: StructureTower | undefined | null = Game.getObjectById("604fe9fbf201b678adf2c70f");
+  if (tower) {
+    var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+    if (closestHostile) {
+      tower.attack(closestHostile);
+    } else {
+      var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
+        filter: structure => {
+          return structure.hits < structure.hitsMax && structure.structureType != STRUCTURE_WALL;
+        }
+      });
+      if (closestDamagedStructure) {
+        tower.repair(closestDamagedStructure);
+      }
     }
+  }
 
+  // var joe = Game.creeps["joe"];
+  // joe.moveTo(Game.flags["joe"])
 
+  for (var roomName in Memory.rooms) {
+    var room: Room = Game.rooms[roomName];
+    if (room) MemoryManager.updateRoom(room);
+  }
+  MemoryManager.removeMissing();
+  RoomPainter.drawAll();
+  SpawnManager.spawn(Game.spawns["Spawn1"]);
 
-
-    var tower: StructureTower | undefined | null = Game.getObjectById("604fe9fbf201b678adf2c70f");
-    if (tower) {
-        var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
-            filter: (structure) => {
-                return (structure.hits < structure.hitsMax
-                    && structure.structureType != STRUCTURE_WALL);
-            },
-        });
-        if (closestDamagedStructure) {
-            tower.repair(closestDamagedStructure);
-        }
-        var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
-        if (closestHostile) {
-            tower.attack(closestHostile);
-        }
+  for (var name in Game.creeps) {
+    var creep = Game.creeps[name];
+    if (creep.memory.role == "harvester") {
+      Harvester.run(creep);
     }
-
-    // var joe = Game.creeps["joe"];
-    // joe.moveTo(Game.flags["joe"])
-
-    for (var roomName in Memory.rooms) {
-        var room: Room = Game.rooms[roomName];
-        if (room)
-            MemoryManager.updateRoom(room);
+    if (creep.memory.role == "upgrader") {
+      Upgrader.run(creep);
     }
-    MemoryManager.removeMissing();
-    RoomPainter.drawAll();
-    SpawnManager.spawn(Game.spawns["Spawn1"]);
-
-    for (var name in Game.creeps) {
-        var creep = Game.creeps[name];
-        if (creep.memory.role == "harvester") {
-            Harvester.run(creep);
-        }
-        if (creep.memory.role == "upgrader") {
-            Upgrader.run(creep);
-        }
-        if (creep.memory.role == "builder") {
-            Builder.run(creep);
-        }
-        if (creep.memory.role == "repairer") {
-            Repairer.run(creep);
-        }
-        if (creep.memory.role == "miner") {
-            Miner.run(creep);
-        }
-        if (creep.memory.role == "haulier") {
-            Haulier.run(creep);
-        }
-        if (creep.memory.role == "claimer") {
-            Claimer.run(creep);
-        }
+    if (creep.memory.role == "builder") {
+      Builder.run(creep);
     }
+    if (creep.memory.role == "repairer") {
+      Repairer.run(creep);
+    }
+    if (creep.memory.role == "miner") {
+      Miner.run(creep);
+    }
+    if (creep.memory.role == "haulier") {
+      Haulier.run(creep);
+    }
+    if (creep.memory.role == "claimer") {
+      Claimer.run(creep);
+    }
+  }
 });
 
 global.STATE_MINING = "mining";
