fe8c93d40570932cdfd015da575b1fea21f4230a
CodyAdam <cody.ad4@gmail.com>
2021-03-17 18:06:11 +0100

    Created helper file and fix rick roll

--- src/role/messenger.ts
+++ src/role/messenger.ts
@@ -1,66 +1,40 @@
 export abstract class Messenger {
-    public static rickRollLyrics: string[] = ["We're no strangers to love",
-        "You know the rules and so do I",
-        "A full commitment's what I'm thinking of",
-        "You wouldn't get this from any other guy",
-        "I just wanna tell you how I'm feeling",
-        "Gotta make you understand",
-        "Never gonna give you up",
-        "Never gonna let you down",
-        "Never gonna run around and desert you",
-        "Never gonna make you cry",
-        "Never gonna say goodbye",
-        "Never gonna tell a lie and hurt you",
-        "We've known each other for so long",
-        "Your heart's been aching, but",
-        "You're too shy to say it",
-        "Inside, we both know what's been going on",
-        "We know the game and we're gonna play it",
-        "And if you ask me how I'm feeling",
-        "Don't tell me you're too blind to see",
-        "Never gonna give you up",
-        "Never gonna let you down",
-        "Never gonna run around and desert you",
-        "Never gonna make you cry",
-        "Never gonna say goodbye",
-        "Never gonna tell a lie and hurt you",
-        "Never gonna give you up",
-        "Never gonna let you down",
-        "Never gonna run around and desert you",
-        "Never gonna make you cry",
-        "Never gonna say goodbye",
-        "Never gonna tell a lie and hurt you",
-        "(Ooh, give you up)",
-        "(Ooh, give you up)",
-        "Never gonna give, never gonna give",
-        "(Give you up)",
-        "Never gonna give, never gonna give",
-        "(Give you up)",
-        "We've known each other for so long",
-        "Your heart's been aching, but",
-        "You're too shy to say it",
-        "Inside, we both know what's been going on",
-        "We know the game and we're gonna play it",
-        "I just wanna tell you how I'm feeling",
-        "Gotta make you understand",
-        "Never gonna give you up",
-        "Never gonna let you down",
-        "Never gonna run around and desert you",
-        "Never gonna make you cry",
-        "Never gonna say goodbye",
-        "Never gonna tell a lie and hurt you",
-        "Never gonna give you up",
-        "Never gonna let you down",
-        "Never gonna run around and desert you",
-        "Never gonna make you cry",
-        "Never gonna say goodbye",
-        "Never gonna tell a lie and hurt you",
-        "Never gonna give you up",
-        "Never gonna let you down",
-        "Never gonna run around and desert you",
-        "Never gonna make you cry",
-        "Never gonna say goodbye",
-        "Never gonna tell a lie and hurt you",]
+    public static rickRollLyrics: string[] = ["We're no",
+        "strangers",
+        "to love",
+        "You know",
+        "the rules",
+        "and so do",
+        "I! 🖤",
+        "*heavy",
+        "breathing*",
+        "Never",
+        "gonna",
+        "give you",
+        "up 😎",
+        "Never",
+        "gonna",
+        "let you",
+        "down ⏬",
+        "Never",
+        "gonna run",
+        "around",
+        "and",
+        "desert",
+        "you 👌",
+        "Never",
+        "gonna",
+        "make you",
+        "cry 🙈",
+        "Never",
+        "gonna say",
+        "goodbye 🙉",
+        "Never",
+        "gonna",
+        "tell a",
+        "lie and",
+        "hurt you!",
+        "👌😎✌"]
 
     public static run(creep: Creep): void {
         if (!creep.memory.target) {
@@ -68,7 +42,7 @@ export abstract class Messenger {
             this.run(creep);
         }
         else {
-            creep.say(this.rickRoll(creep.memory.target.tick));
+            creep.say(this.rickRoll(creep.memory.target.tick), true);
             this.incrementTick(creep);
             this.move(creep);
         }
@@ -80,7 +54,7 @@ export abstract class Messenger {
 
     public static incrementTick(creep: Creep): void {
         if (creep.memory.target)
-            creep.memory.target = { tick: creep.memory.target.tick + 1, goToFlag: creep.memory.target.goToFlag };
+            creep.memory.target = { tick: creep.memory.target.tick + 1, flagTick: creep.memory.target.flagTick };
     }
 
     public static move(creep: Creep): void {
@@ -90,11 +64,12 @@ export abstract class Messenger {
             );
         })
         flagsRoute = _.sortBy(flagsRoute, (flag: Flag) => { return (flag.name); })
-
-        if (flagsRoute.length && creep.memory.target.flagTick) {
-            if (flagsRoute[creep.memory.target.flagTick % flagsRoute.length].pos == creep.pos)
-                creep.memory.target.flagTick += 1
-            creep.moveTo(flagsRoute[creep.memory.target.flagTick % flagsRoute.length])
+        if (flagsRoute.length && creep.memory.target.flagTick != undefined && creep.memory.target.flagTick != null) {
+            let target: RoomPosition = flagsRoute[creep.memory.target.flagTick % flagsRoute.length].pos
+            if (target.x == creep.pos.x && target.y == creep.pos.y && target.roomName == creep.pos.roomName) {
+                creep.memory.target = { tick: creep.memory.target.tick, flagTick: creep.memory.target.flagTick + 1 };
+            }
+            creep.moveTo(flagsRoute[creep.memory.target.flagTick % flagsRoute.length], { visualizePathStyle: { stroke: "#fcf803" } })
         }
     }
 }
