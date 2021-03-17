e890962a495a157458ba8f0d282483fe32048563
CodyAdam <cody.ad4@gmail.com>
2021-03-17 16:49:31 +0100

    Clean code

--- src/spawnManager.ts
+++ src/spawnManager.ts
@@ -8,28 +8,19 @@ export class SpawnManager {
     if (!spawn.spawning && _.size(Game.creeps) < this.maxCount) {
       const role = this.neededRole();
       if (role) {
-        let cost = 0;
         let bodyParts: BodyPartConstant[] = [WORK, CARRY, MOVE];
         switch (role) {
           case "miner":
             bodyParts = [WORK, WORK, WORK, WORK, WORK, MOVE, MOVE, MOVE, MOVE];
-            output += " miner spawned";
-            cost = 700;
             break;
           case "builder":
             bodyParts = [WORK, WORK, WORK, WORK, CARRY, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE];
-            output += " builder spawned";
-            cost = 800;
             break;
           case "upgrader":
             bodyParts = [WORK, WORK, WORK, WORK, WORK, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE];
-            output += " upgrader spawned";
-            cost = 800;
             break;
           case "repairer":
             bodyParts = [WORK, WORK, CARRY, CARRY, MOVE, MOVE];
-            output += " repairer spawned";
-            cost = 400;
             break;
           case "haulier":
             bodyParts = [
@@ -50,18 +41,20 @@ export class SpawnManager {
               CARRY,
               CARRY
             ];
-            cost = 800;
             break;
           case "claimer":
             bodyParts = [CLAIM, WORK, MOVE, MOVE];
-            cost = 800;
+            break;
+          case "messenger":
+            bodyParts = [MOVE, MOVE];
             break;
           default:
             return "role not found";
         }
+        const cost = _.sum(bodyParts, (b) => BODYPART_COST[b]);
         if (spawn.room.energyAvailable < cost) return "not enough energy";
         output += role + " spawned";
-        const name: string = role + (Game.time - 26401107);
+        const name: string = role + (Game.time - 26470107);
         spawn.spawnCreep(bodyParts, name, {
           memory: {
             target: null,
